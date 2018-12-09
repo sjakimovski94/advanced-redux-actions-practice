f2b2dc14e37edae1c04ee1b472b576f3e4788ccb
Stefan Jakimovski <s.jakimovski94@yahoo.com>
2018-12-09 17:36:11 -0600

    SpecialText.js

--- src/components/SpecialText.js
+++ src/components/SpecialText.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 function SpecialText(props) {
   return (
@@ -7,4 +8,11 @@ function SpecialText(props) {
       </div>
   );
 }
-export default SpecialText;
+
+function mapStateToProps(state){
+  return {
+    text: state.specialText
+  }
+}
+
+export default connect(mapStateToProps)(SpecialText);
