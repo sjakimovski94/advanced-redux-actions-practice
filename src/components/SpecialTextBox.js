2654cd104ffbdf5f56ea46224bbb58054c6a0b5e
Stefan Jakimovski <s.jakimovski94@yahoo.com>
2018-12-09 17:36:41 -0600

    SpecialTextBox.js

--- src/components/SpecialTextBox.js
+++ src/components/SpecialTextBox.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from "react-redux";
 import {setSpecialText} from "../actions";
 
+
 function SpecialTextBox(props) {
   return (
       <div>
@@ -17,10 +18,11 @@ function SpecialTextBox(props) {
 
 function mapDispatchToProps(dispatch){
   return {
-    set:function(txt){
-      let action = setSpecialText(txt)
-      dispatch(action);
-    }
+   set:function(txt){
+     let action = setSpecialText(txt);
+     dispatch(action);
+   }
   }
 }
-export default (SpecialTextBox);
+
+export default connect(null,mapDispatchToProps)(SpecialTextBox);
