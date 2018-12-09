36696e452f8844ab621b00cbd5f52042f254385c
Stefan Jakimovski <s.jakimovski94@yahoo.com>
2018-12-09 17:48:55 -0600

    index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -31,7 +31,65 @@ function specialText(state = "", action){
   return state;
 }
 
+function currentTemp(state = "", action){
+  if(action.type === "SET_TEMP"){
+    return action.value;
+  }
+  return state;
+}
+
+function currentCity(state = "", action){
+  if(action.type === "SET_CURRENT_CITY"){
+    return action.value;
+  }
+  return state;
+}
+
+function isLoading(state = "", action){
+  if(action.type === "SET_IS_LOADING"){
+    return action.value;
+  }
+  return state;
+}
+
+function videoURL(state = "", action){
+  if(action.type === "SET_VIDEO_URL"){
+    return action.value;
+  }
+  return state;
+}
+
+function searchText(state = "", action){
+  if(action.type === "SET_SEARCH_TEXT"){
+    return action.value;
+  }
+  return state;
+}
+
+function currentUserSort(state = "", action){
+  if(action.type === "SET_CURRENT_USER_SORT"){
+    return action.value;
+  }
+  return state;
+}
+
+function videoScale(state = "", action){
+  if(action.type === "SET_VIDEO_SCALE"){
+    return action.value;
+  }
+  return state;
+}
+
 const rootReducer = combineReducers({
-  currentCount, users, specialText
+  currentCount,
+  users,
+  specialText,
+  currentTemp, 
+  currentCity, 
+  isLoading, 
+  videoURL, 
+  searchText,
+  currentUserSort, 
+  videoScale
  });
  export default rootReducer;
